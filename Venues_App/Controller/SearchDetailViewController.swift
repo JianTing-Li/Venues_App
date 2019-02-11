8638dd14a22a897be30b23ce9838fc8176af7c51
Manny Yusuf <manolovayusuf@pursuit.org>
2019-02-11 12:24:36 -0500

    Setup Files and Tabs

---
+++ Venues_App/Controller/SearchDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchDetailViewController.swift
+//  Venues_App
+//
+//  Created by Manny Yusuf on 2/11/19.
+//  Copyright © 2019 Jian Ting Li. All rights reserved.
+//
+
+import UIKit
+
+class SearchDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
---
+++ Venues_App/Controller/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  Venues_App
+//
+//  Created by Manny Yusuf on 2/11/19.
+//  Copyright © 2019 Jian Ting Li. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabBarController?.tabBar.items?[1].image = UIImage(named: "settings")
+        self.view.backgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+        title = "Settings"
+    }
+}
