f657bde827cdcf3fdec0a8d505e3ad1c2a3fbdd0
Manny Yusuf <manolovayusuf@pursuit.org>
2019-02-12 18:03:43 -0500

    Finished UI and UV

--- Venues_App/Controller/SearchViewController.swift
+++ Venues_App/Controller/SearchViewController.swift
@@ -10,10 +10,21 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    public var searchView = SearchView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBarController?.tabBar.items?[0].image = UIImage(named: "search")
         self.view.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
         title = "Search"
+        view.addSubview(searchView)
+        let MapButton = UIBarButtonItem.init(title: "Map", style: .plain, target: self, action: #selector(MapButtonPressed))
+        self.navigationItem.rightBarButtonItem = MapButton
     }
+    
+    @objc func MapButtonPressed() {
+        
+    }
+    
 }
+
