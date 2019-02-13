a8cdee2aa857a3320b3e59e2eec30bb49ffe9b3b
AaronCab <Aaron@Pursuits-MacBook-Pro-17.local>
2019-02-13 15:38:40 -0500

    Venues Model and API

--- Venues_App/Controller/SearchViewController.swift
+++ Venues_App/Controller/SearchViewController.swift
@@ -9,11 +9,19 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    var venues: [Venue]!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBarController?.tabBar.items?[0].image = UIImage(named: "search")
         self.view.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
         title = "Search"
+        ApiClient.getVenue { (error, data) in
+            if let error = error {
+                print(error.errorMessage())
+            } else if let data = data {
+                self.venues = data
+                dump(self.venues)
+            }
+        }
     }
 }
--- Venues_App/Network Services/APIClient.swift
+++ Venues_App/Network Services/APIClient.swift
@@ -9,5 +9,19 @@
 import Foundation
 
 final class ApiClient {
-    
+    static func getVenue(completionHandler: @escaping (AppError?, [Venue]?) -> Void) {
+        let urlString = "https://api.foursquare.com/v2/venues/search?ll=40.7484,-73.9857&client_id=GTFB0TZVWRZNRGCB0HYEJVVXTY2BHGWO2AD44UUA2TZMXGD1&client_secret=SUGBKQVN5I5ZNAKIH10EBR3D3YTQOR2GBHBUGAC0FUGGWCLS&v=20190201"
+        NetworkHelper.shared.performDataTask(endpointURLString: urlString) { (error, data) in
+            if let error = error {
+                completionHandler(error, nil)
+            } else if let data = data {
+                do {
+                    let somevenues = try JSONDecoder().decode(FourSquareData.self, from: data)
+                    completionHandler(nil, somevenues.response.venues)
+                } catch {
+                    completionHandler(AppError.jsonDecodingError(error), nil)
+                }
+            }
+        }
+    }
 }
