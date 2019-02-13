82e3f3a25eb8128101cbde0731fff020c0154024
kwaring3 <kevinwaring@pursuit.org>
2019-02-13 15:47:50 -0500

    Finished Views , adding cells

--- Venues_App/View/SettingsView.swift
+++ Venues_App/View/SettingsView.swift
@@ -18,6 +18,14 @@ class SettingsView: UIView {
         let textField = UITextField()
         return textField
     }()
+    lazy var label1: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    lazy var label2: UILabel = {
+        let label = UILabel()
+        return label
+    }()
     override init(frame: CGRect) {
         super.init(frame: UIScreen.main.bounds)
         commonInit()
@@ -36,6 +44,8 @@ class SettingsView: UIView {
     private func setUpViews() {
         setupTextView()
         setupTextView2()
+        setupLabel1()
+        setupLabel2()
     }
     private func setupTextView() {
         addSubview(textField1)
@@ -54,6 +64,30 @@ class SettingsView: UIView {
         textField2.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 50).isActive = true
         textField2.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -50).isActive = true
         textField2.heightAnchor.constraint(equalToConstant: 40).isActive = true
+    }
+    private func setupLabel1() {
+        addSubview(label1)
+        label1.backgroundColor = .green
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        //label1.topAnchor.constraint(equalTo: topAnchor, constant: 300).isActive = true
+        label1.bottomAnchor.constraint(equalTo: textField1.topAnchor, constant: -8).isActive = true
+        label1.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 50).isActive = true
+        label1.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -150).isActive = true
+        label1.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        
+        
+    }
+    private func setupLabel2() {
+        addSubview(label2)
+        label2.backgroundColor = .green
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        //label1.topAnchor.constraint(equalTo: topAnchor, constant: 300).isActive = true
+        label2.bottomAnchor.constraint(equalTo: textField2.topAnchor, constant: -8).isActive = true
+        label2.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 50).isActive = true
+        label2.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -150).isActive = true
+        label2.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        
+        
     }
   
 }
